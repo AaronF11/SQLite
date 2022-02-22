31f27062f402ed0a215f64ec92ebb77a051ee6c4
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2022-02-22 00:25:51 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data;
+
+namespace SQLite
+{
+    public partial class DlgPrincipal : Form
+    {
+        //Conexión a la Base de datos.
+        public SQLiteConnection connection = new SQLiteConnection("Data source = C:/Users/aaron/source/repos/SQLite/src/db.db");
+        
+        public DlgPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            //Consultar contenido de tabla alumnos.
+            SQLiteDataAdapter DataAdapter = new SQLiteDataAdapter("SELECT * FROM alumnos", connection);
+
+            //Craer datatable.
+            DataTable Dt = new DataTable();
+
+            //Llenado de datos al datatable.
+            DataAdapter.Fill(Dt);
+
+            //Agregar datos al DataGridView.
+            DgvDatos.DataSource = Dt;
+        }
+    }
+}
